4539fedfdf39baf324bef30a44b2a4009a2801c6
agilles1 <adam.g.tpt@gmail.com>
2021-01-27 23:11:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(a, holiday){
+    for(let i=0; i<a.length; i++) {
+        a[i] = `Thank you, ${a[i]}, for the wonderful ${holiday} gift!`;
+    }
+    return a
+}
+
+function countDown(n){
+    while (n >= 0){
+        console.log(n)
+        n--
+    }
+}
